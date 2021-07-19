ec6c97be228d152085f46b1bdf86b6b998181683
Bhanuthug <ab.prathyush@gmail.com>
2021-07-19 15:21:19 +0530

    Done

--- 06-eggcartons-Python/eggcartons.py
+++ 06-eggcartons-Python/eggcartons.py
@@ -3,7 +3,17 @@
 # the smallest integer number of cartons required to hold 
 # that many eggs, where a carton may hold up to 12 eggs
 
-
 def fun_eggcartons(eggs):
-	# your code goes here
-	return 1
+    	# your code goes here
+	b = eggs
+	if b > 0 and b <=12:
+		return 1
+	elif b > 12 and b <= 24:
+		return 2
+	elif b > 24 and b <= 36:
+		return 3
+	elif b > 36:
+		return 4
+	else:
+		return 0
+	
