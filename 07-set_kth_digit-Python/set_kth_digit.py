799317c3fdff2d55fd20dfce0f6216fa5a35f533
Bhanuthug <ab.prathyush@gmail.com>
2021-07-19 15:30:40 +0530

    Done

--- 07-set_kth_digit-Python/set_kth_digit.py
+++ 07-set_kth_digit-Python/set_kth_digit.py
@@ -5,7 +5,26 @@
 # so the 0th digit is the rightmost digit. 
 
 
-
 def fun_set_kth_digit(n, k, d):
-		return 0
+    flag = False
+    if(n < 0):
+        n = -n
+        flag = True
+    count = 0
+    res = 0
+    while(count<=k or n > 0):
+        r = n % 10
+        if count == k:
+            res = res + (d * (10 ** count))
+        else:
+            res = res + (r * (10 ** count))
+        count += 1
+        n = n // 10
+    
+    if flag:
+        res = -res
+   
+    return res
+   
+
 
