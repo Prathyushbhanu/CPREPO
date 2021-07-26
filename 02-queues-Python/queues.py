68332ae47ac0de6a2e43efd2bf8f1bd23c31c777
Bhanuthug <ab.prathyush@gmail.com>
2021-07-26 16:33:00 +0530

    Done

--- 02-queues-Python/queues.py
+++ 02-queues-Python/queues.py
@@ -3,16 +3,16 @@ Hint: You can use any Python list method
 you'd like! Try to write each one in as 
 few lines as possible.
 Make sure you pass the test cases too!"""
-
 class Queue:
-    def __init__(self, head=None):
-        self.storage = [head]
+    def init(self, head=None):
+        self.list = [head]
+
 
     def enqueue(self, new_element):
-        pass
-
-    def peek(self):
-        pass 
+        return self.list.append(new_element)
 
     def dequeue(self):
-        pass
+        return self.list.pop(0)
+
+    def peek(self):
+        return (self.list[0])
