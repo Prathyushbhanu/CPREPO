b9c1be10a53d002a2f3a3227e4ffdc87eefada24
Bhanuthug <ab.prathyush@gmail.com>
2021-07-26 15:09:27 +0530

    Done

---
+++ 07-isfactor-Python/isfact.py
@@ -0,0 +1,16 @@
+# Write the function isFactor(f, n) that takes 
+# two int values f and n, and returns True 
+# if f is a factor of n, and False otherwise. 
+# Note that every integer is a factor of 0.
+
+def fun_isfactor(f,n):
+    if (n % f == 0):
+        return True
+    elif ( f == 0):
+        return False
+    elif ( n == 0 and f == 0):
+        return True
+    else:
+        return False
+
+print(fun_isfactor(int(input()), int(input())))
--- 14-islegaltriangle-Python/islegaltriangle.py
+++ 14-islegaltriangle-Python/islegaltriangle.py
@@ -5,6 +5,12 @@
 # than the third side, and further note that all sides of a legal triangle must be positive. Hint:
 # how can you determine the longest side, and how might that help?
 
-def islegaltriangle(s1, s2, s3):
-	# your code goes here
-	pass
+def trianglearea(s1, s2, s3):
+    
+	s=(s1+s2+s3)/2	
+	b=s-s1
+	c=s-s2
+	d=s-s3
+	e=(s*b*c*d)**0.5
+	# print(e)
+	# b=a**0.5
