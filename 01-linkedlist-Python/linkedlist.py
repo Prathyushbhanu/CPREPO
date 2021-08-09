786ef681f4a2c758f6c9c2f1282effa40fbd3269
Prathyushbhanu <72145663+Prathyushbhanu@users.noreply.github.com>
2021-08-09 17:18:39 +0530

    Update linkedlist.py

--- 01-linkedlist-Python/linkedlist.py
+++ 01-linkedlist-Python/linkedlist.py
@@ -31,3 +31,44 @@ class LinkedList(object):
    def get_position(self, position):
        """Get an element from a particular position. Assume the first position is "1". Return "None" if position is not in the list."""
        counter = 1
+       current = self.head
+       if position < 1:
+           return None
+       while current and counter <= position:
+           if counter == position:
+               return current
+           current = current.next
+           counter += 1
+       return None
+    def insert(self, new_element, position):
+       """Insert a new node at the given position. Assume the first position is "1". Inserting at position 3 means between the 2nd and 3rd elements."""
+       flag = 1
+       current = self.head
+       if position > 1:
+           while current and flag < position:
+               if flag == position - 1:
+                   new_element.next = current.next
+                   current.next = new_element
+               current = current.next
+               flag += 1
+       elif position == 1:
+           new_element.next = self.head
+           self.head = new_element
+    def delete(self, value):
+       """Delete the first node with a given value."""
+       current = self.head
+       previous = None
+       while current.value != value and current.next:
+           previous = current
+           current = current.next
+       if current.value == value:
+           if previous:
+               previous.next = current.next
+           else:
+               self.head = current.next
+              
+   def display(self):
+       current = self.head
+       while current != None:
+           print(current.value)
+           current =current.next
