983daf863314849063da320c63d4cb7f3b671fe1
Prathyushbhanu <72145663+Prathyushbhanu@users.noreply.github.com>
2021-08-09 17:21:52 +0530

    Update nth_palindromic_prime.py

--- 09-nth_palindromic_prime-Python/nth_palindromic_prime.py
+++ 09-nth_palindromic_prime-Python/nth_palindromic_prime.py
@@ -1,11 +1,36 @@
-# Write the function fun_nth_palindromic_prime(n) that takes 
-# a non-negative int n and returns the nth palindromic Prime, 
-# which is a palidrome number such that 
-# it is also a prime. 
-
-# For example, 313 is a palindrome and it is prime 
-# so 313 is an palindrome Prime. fun_nth_palindrome_prime(0) 
-# returns 2
-
-def fun_nth_palindromic_prime(n):
-	return 0
+# Write a function isPalindromicPrime that takes a value n as a parameter and returns True if the given n is a palindrome and prime and False otherwise.
+ 
+def isPalindromicPrime(a):
+    if (isPalindrome(a) and isPrime(a)):
+        return True
+    else:
+        return False
+ 
+def isPalindrome(n):
+    n=str(n)
+    return n[::-1]==n
+ 
+def isPrime(n):
+    if n < 2:
+        return False
+    elif n == 2:
+        return True
+    elif n % 2 == 0:
+        return False
+    else:
+        for i in range(3, n):
+            if n % i == 0:
+                return False
+        return True
+ 
+assert (isPalindromicPrime(2) == True)
+assert (isPalindromicPrime(10) == False)
+assert (isPalindromicPrime(104) == False)
+assert (isPalindromicPrime(235) == False)
+assert (isPalindromicPrime(131) == True)
+assert (isPalindromicPrime(900) == False)
+assert (isPalindromicPrime(101) == True)
+assert (isPalindromicPrime(383) == True)
+assert (isPalindromicPrime(373) == True)
+assert (isPalindromicPrime(121) == False)
+print("All test cases passed... :-)")
