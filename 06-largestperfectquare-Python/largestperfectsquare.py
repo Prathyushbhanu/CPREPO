713ea6b53ea538e28908bebd86f940c541f99558
Prathyushbhanu <72145663+Prathyushbhanu@users.noreply.github.com>
2021-08-09 16:52:18 +0530

    Update largestperfectsquare.py

--- 06-largestperfectquare-Python/largestperfectsquare.py
+++ 06-largestperfectquare-Python/largestperfectsquare.py
@@ -1,12 +1,12 @@
-# largestPerfectSquare(n) [10 pts]
-# Write the function largestPerfectSquare(n) that takes a non-negative int n, and returns  the largest perfect
-# square that is no larger than n. For example:
+# Write the function largestPerfectSquare(n) that takes a non-negative int n, and returns  the largest perfectsquare that is no larger than n. For example:
 # assert(largestPerfectSquare(24) == 16)
 # assert(largestPerfectSquare(25) == 25)
 # assert(largestPerfectSquare(26) == 25)
-# Hint: you may wish to use a similar approach to how you solved isPerfectSquare on the hw.
-# Another hint: This can be written using just one or two lines of Python.
-
+# Hint: you may wish to use a similar approach to how you solved isPerfectSquare on the hw.Another hint: This can be written using just one or two lines of Python.
 def largestperfectsquare(n):
-	# your code goes here
-	pass
+    a=[]
+    for i in range(1,n+1):
+        if(i**(0.5) == int(i**(0.5)) and i<=n):
+            a.append(i)
+    b = a[::-1]
+    return b[0]
