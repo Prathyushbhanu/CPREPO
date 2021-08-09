1ab8e2b499c65559f1c4a9c25d79cafa1a5245cc
Prathyushbhanu <72145663+Prathyushbhanu@users.noreply.github.com>
2021-08-09 17:17:36 +0530

    Update linkedlist.py

--- 01-linkedlist-Python/linkedlist.py
+++ 01-linkedlist-Python/linkedlist.py
@@ -8,36 +8,26 @@ particular value.
 Then, use "Test Run" and "Submit" to run the test cases
 at the bottom."""
 
+
 class Element(object):
-    def __init__(self, value):
-        self.value = value
-        self.next = None
-        
+   def __init__(self, value):
+       self.value = value
+       self.next = None
+    
 class LinkedList(object):
-    def __init__(self, head=None):
-        self.head = head
+   def __init__(self, head=None):
+       self.head = head
+    
+   def append(self, new_element):
+       # Your code goes here
+       current = self.head
+       if self.head:
+           while current.next:
+               current = current.next
+           current.next = new_element
+       else:
+           self.head = new_element
         
-    def append(self, new_element):
-        # Your code goes here
-        pass
-            
-    def get_position(self, position):
-        """Get an element from a particular position.
-        Assume the first position is "1".
-        Return "None" if position is not in the list."""
-        # Your code goes here
-        pass
-    
-    def insert(self, new_element, position):
-        """Insert a new node at the given position.
-        Assume the first position is "1".
-        Inserting at position 3 means between
-        the 2nd and 3rd elements."""
-        # Your code goes here
-        pass
-    
-    
-    def delete(self, value):
-        """Delete the first node with a given value."""
-        # Your code goes here
-        pass
+   def get_position(self, position):
+       """Get an element from a particular position. Assume the first position is "1". Return "None" if position is not in the list."""
+       counter = 1
